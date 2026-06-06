99a474d0660034bc9c1a0fbf4b3ca88be0c672b5
Fabricio <fabriga1509@gmail.com>
2026-06-06 13:26:03 -0600

    Se crearon las colisiones de la ventana

--- entities/player.py
+++ entities/player.py
@@ -1,12 +1,12 @@
 import pygame 
-from constants import PLAYER_SPEED, PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER_COLOR
-class Player:
-    def __init__(self,x,y,teclas):
+from constants import PLAYER_SPEED, PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER_COLOR, ancho_ventana, alto_ventana #Importamos las constantes necesarias para el jugador
+class Player: #Creamos la clase Player para representar a los chefs en el juego
+    def __init__(self,x,y,teclas): #El constructor recibe la posición inicial del jugador y las teclas asignadas para controlar al jugador
         self.rect = pygame.Rect(x,y, PLAYER_WIDTH, PLAYER_HEIGHT)
         self.velocidad = PLAYER_SPEED
         self.ingrediente = None
         self.teclas = teclas
-    def mover(self,keys):
+    def mover(self,keys): #El método mover recibe las teclas presionadas y actualiza la posición del jugador en función de las teclas asignadas
         if keys[self.teclas['Izquierda']]:
             self.rect.x -= self.velocidad
         if keys[self.teclas['Derecha']]:
@@ -15,20 +15,29 @@ class Player:
             self.rect.y -= self.velocidad
         if keys[self.teclas['Abajo']]:
             self.rect.y += self.velocidad
-    def recoger(self,estación):
+        #Limitar dentro de la ventana
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > ancho_ventana:
+            self.rect.right = ancho_ventana
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > alto_ventana:
+            self.rect.bottom = alto_ventana
+    def recoger(self,estación): #El método recoger recibe una estación y recoge el ingrediente de esa estación, asignándolo al jugador
         self.ingrediente = estación.ingrediente
-    def soltar(self,estación):
+    def soltar(self,estación): #El método soltar recibe una estación y suelta el ingrediente que el jugador tiene en esa estación
         estación.ingrediente = self.ingrediente
         self.ingrediente = None
-    def interactuar(self,estaciones):
+    def interactuar(self,estaciones): #Metodo para interactuar con las estaciones
         for estacion in estaciones:
-            if self.rect.colliderect(estacion.rect):
+            if self.rect.colliderect(estacion.rect): #Verificamos si el jugador está colisionando con alguna estación
                 if self.ingrediente is None:
-                    self.recoger(estacion)
+                    self.recoger(estacion) #Si el jugador no tiene un ingrediente, lo recoge de la estación
                 else:
-                    self.soltar(estacion)
+                    self.soltar(estacion) #Si el jugador tiene un ingrediente, lo suelta en la estación
                 break
-    def update(self,keys):
+    def update(self,keys): #El método update recibe las teclas presionadas y llama al método mover para actualizar la posición del jugador
         self.mover(keys)
-    def draw(self, screen):
-        pygame.draw.rect(screen, PLAYER_COLOR, self.rect)
+    def draw(self, screen): #El método draw recibe la pantalla y dibuja al jugador
+        pygame.draw.rect(screen, PLAYER_COLOR, self.rect) #Abi aquí puedes modificar el dibujo del jugador, cuando ya tengas el sprite cambia el rectángulo por el sprite del chef
--- game.py
+++ game.py
@@ -1,9 +1,9 @@
 import pygame
 import sys
-from constants import ancho_ventana, alto_ventana, fps, titulo, white, CHEF1_TECLAS, CHEF2_TECLAS
-from entities.player import Player
+from constants import ancho_ventana, alto_ventana, fps, titulo, white, CHEF1_TECLAS, CHEF2_TECLAS #Importamos las constantes necesarias para el juego
+from entities.player import Player #Importamos la clase Player para crear a los chefs en el juego
 class Game:
-    def __init__(self):
+    def __init__(self): #Metodo constructor para inicializar el juego, crear la ventana, el reloj y los jugadores
         pygame.init()
         self.screen = pygame.display.set_mode((ancho_ventana,alto_ventana))
         pygame.display.set_caption(titulo)
@@ -11,7 +11,7 @@ class Game:
         self.running = True
         self.chef1 = Player(100,100,CHEF1_TECLAS)
         self.chef2 = Player(200,100,CHEF2_TECLAS)
-    def handle_events(self):
+    def handle_events(self): #Metodo para manejar los eventos del juego, como cerrar la ventana o interactuar con las estaciones
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
@@ -20,16 +20,16 @@ class Game:
                     self.chef1.interactuar(self.estaciones)
                     if event.key == self.chef2.teclas["Interactuar"]:
                         self.chef2.interactuar(self.estaciones)
-    def update(self):
+    def update(self): #Metodo para actualizar el estado del juego, como la posición de los jugadores
         keys = pygame.key.get_pressed()
         self.chef1.update(keys)
         self.chef2.update(keys)
-    def draw(self):
+    def draw(self): #Metodo para dibujar los elementos del juego en la pantalla, como los jugadores y las estaciones
         self.screen.fill(white)
         self.chef1.draw(self.screen)
         self.chef2.draw(self.screen)
         pygame.display.flip()
-    def run(self):
+    def run(self): #Metodo principal del juego
         while self.running:
             self.handle_events()
             self.update()
